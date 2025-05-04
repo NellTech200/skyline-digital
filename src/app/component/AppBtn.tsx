1add83a03c7c0757083a3619740d91d3685d33ba
Tony <gaonelleng@gmail.com>
2025-05-04 01:56:11 +0100

    first bug

--- src/app/component/AppBtn.tsx
+++ src/app/component/AppBtn.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
 import './appBtn.css';
 
-export default function AppBtn({name}: {name: string}) {
+export default function AppBtn({ name }: { name: string }) {
 
     const handleScrollTo = (section: string) => {
-        // go to booking a table section
+        const targetSection = document.getElementById(section);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
     }
 
-    return(
-    <a 
-     
-     className='app-btn scrollto d-none d-lg-flex'
-     onClick={() => handleScrollTo('book-a-table')}
-    >
-        {name}
-    </a>
-  );
+    return (
+        <a
+            className='app-btn scrollto d-none d-lg-flex'
+            onClick={() => handleScrollTo('book-a-table')}
+        >
+            {name}
+        </a>
+    );
 }
